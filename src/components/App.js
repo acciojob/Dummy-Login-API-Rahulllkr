e4ca4a40cf89a718be4371c7f115a3b6ac1885d2
Rahulllkr <rahulkumar8604745@gmail.com>
2025-10-16 16:32:16 +0530

    :check

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,36 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState } from 'react'
 
+const App = () => {
+
+    const [email,setPassword] = useState("")
+    const [password,setPassword] = useState("")
+    const data = [
+    {
+        id: 1,
+        name: "ABC",
+        email: "abc@gmail.com",
+        password: "12"
+    },
+    {
+        id: 2,
+        name: "DEF",
+        email: "def@gmail.com",
+        password: "1234"
+    },
+    {
+        id: 3,
+        name: "GHI",
+        email: "ghi@gmail.com",
+        password: "123456"
+    }
+]
+  return (
+    <div>
+        <input id='input-email' />
+        <input id='input-password' />
+        <button onClick={handleInput} id='submit-form-btn'>Submit</button>
+    </div>
+  )
+}
+
+export default App
